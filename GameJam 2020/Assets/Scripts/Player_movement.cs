2292b6e42025d2f57d4b14efec88b8a02a8bdcdc
roy017 <41708735+roy017@users.noreply.github.com>
2020-08-01 18:52:53 -0700

    Blob Added
    Added Blob character,
    added folders, to be more organized,
    added idle animation for Blob,
    added movement script for Blob

---
+++ GameJam 2020/Assets/Scripts/Player_movement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_movement : MonoBehaviour
+{
+    public float speed = 1f;
+    
+    private float movement_X;
+    private float movement_Y;
+    private Vector2 movement;
+    private bool facingRight = true;
+
+    public Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement_X = Input.GetAxisRaw("Horizontal");
+        movement_Y = 0;
+        movement = new Vector2(movement_X, movement_Y);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+        if(facingRight == false && movement_X > 0)
+        {
+            flip();
+        }
+        else if(facingRight == true && movement_X < 0)
+        {
+            flip();
+        }
+    }
+
+    void flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+    }
+}
