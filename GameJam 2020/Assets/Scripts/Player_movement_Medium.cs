14b14c099435f4e8d3ff37b3e15a12e5e8f97a15
roy017 <41708735+roy017@users.noreply.github.com>
2020-08-02 17:42:00 -0700

    Added medium blob, and jump mechanic
    Blob medium added + idle & run animation
    Jump mechanic is added, but needs to be tuned and improved.
    Jump animation might be needed.

---
+++ GameJam 2020/Assets/Scripts/Player_movement_Medium.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_movement_Medium : MonoBehaviour
+{
+    public float speed = 1f;
+    public int Total_Jumps = 1;
+    public float jumpForce = 1f;
+
+    private float movement_X;
+    private float movement_Y;
+    private Vector2 movement;
+    private bool facingRight = true;
+
+    public Rigidbody2D rb;
+    public Animator anim;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    private float groundCheckRadius= 0.2f;
+    public LayerMask whatIsGround;
+
+    int cur_jumps = 0;
+    private bool launched;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cur_jumps = Total_Jumps;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement_X = Input.GetAxisRaw("Horizontal");
+        movement_Y = 0;
+        movement = new Vector2(movement_X, movement_Y);
+        anim.SetFloat("speed", Mathf.Abs(movement_X));
+
+        if(rb.velocity.y == 0)
+        {
+            launched = false;
+        }
+
+        if (isGrounded == true && launched == false)
+        {
+            cur_jumps = Total_Jumps;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && cur_jumps > 0)
+        {
+            Jump();
+            launched = true;
+            cur_jumps--;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        //rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+        rb.velocity = new Vector2(movement_X * speed, rb.velocity.y);
+        if (facingRight == false && movement_X > 0)
+        {
+            flip();
+        }
+        else if (facingRight == true && movement_X < 0)
+        {
+            flip();
+        }
+    }
+
+    void flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+    }
+
+    public void Jump()
+    {
+        Vector3 up = transform.TransformDirection(Vector3.up);
+        rb.AddForce(up * jumpForce, ForceMode2D.Impulse);
+    }
+}
