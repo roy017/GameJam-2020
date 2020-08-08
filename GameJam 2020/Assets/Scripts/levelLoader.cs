238a7f391fc9fbe7bd55df8b2ca4f2e29c1d5a08
roy017 <41708735+roy017@users.noreply.github.com>
2020-08-08 00:46:33 -0700

    Lever PNG + script to change to next scene

---
+++ GameJam 2020/Assets/Scripts/levelLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelLoader : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            //Debug.Log("SUPPPPPPP");
+            LoadNextLevel();
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
